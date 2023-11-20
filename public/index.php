c76e84761c1590222913bc141bc90da1819d723f
Jhonathan Nila <nilalimon@gmail.com>
2023-11-20 00:14:08 -0600

    Comienza la zona privada y la pagina de citas

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/../includes/app.php';
 
 use MVC\Router;
 use Controllers\LoginController;
+use Controllers\CitaController;
 
 $router = new Router();
 
@@ -25,6 +26,9 @@ $router->post('/crear-cuenta', [LoginController::class, 'crear']);
 // Confirmación de cuentas
 $router->get('/confirmar-cuenta', [LoginController::class, 'confirmar']);
 $router->get('/mensaje', [LoginController::class, 'mensaje']);
+
+// Área Privada
+$router->get('/cita', [CitaController::class, 'index']);
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
---
+++ views/cita/index.php
@@ -0,0 +1,43 @@
+<h1 class="nombre-pagina">Crear Nueva Cita</h1>
+<p class="descripcion-pagina">Elige tus servicios e ingresa tus datos</p>
+<div id="app">
+    <div class="seccion" id="paso-1">
+        <h2>Servicios</h2>
+            <p class="text-center">Elige tus servicios a continuación</p>
+        <div class="listado-servicios" id="servicios"></div>
+    </div>
+    <div class="seccion" id="paso-2">
+        <h2>Tus Datos y Cita</h2>
+            <p class="text-center">Coloca tus datos y fecha de la cita</p>
+            <form class="formulario">
+                <div class="campo">
+                    <label for="nombre">Nombre</label>
+                        <input 
+                            type="text" 
+                            id="nombre"
+                            placeholder="Tu Nombre"
+                            value="<?php echo $nombre ?>"
+                            disabled
+                        />
+                </div>
+                <div class="campo">
+                    <label for="fecha">Fecha</label>
+                        <input 
+                            type="date" 
+                            id="fecha"
+                        />
+                </div>
+                <div class="campo">
+                    <label for="hora">Hora</label>
+                        <input 
+                            type="time" 
+                            id="hora"
+                        />
+                </div>
+            </form>
+    </div>
+    <div class="seccion" id="paso-3">
+        <h2>Resumen</h2>
+            <p class="text-center">Verifica que la información sea correcta</p>
+    </div>
+</div>

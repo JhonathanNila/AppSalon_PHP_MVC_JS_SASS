3ccfad0816a9ec547c38160de6043444f063a549
Jhonathan Nila <nilalimon@gmail.com>
2023-11-21 21:44:42 -0600

    Se termina la sección de navtab

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,35 @@
-alert('Desde app.js');
+let paso = 1;
+
+document.addEventListener('DOMContentLoaded', function() {
+    iniciarApp();
+});
+
+function iniciarApp() {
+    mostrarSeccion(); //Muestra y oculta las secciones
+    tabs(); //Cambia la sección cuando se presione los tabs
+}
+function mostrarSeccion() {
+    const seccionAnterior = document.querySelector('.mostrar');
+    if(seccionAnterior) {
+        seccionAnterior.classList.remove('mostrar');
+    }
+    const pasoSelector = `#paso-${paso}`;
+    const seccion = document.querySelector(pasoSelector);
+    seccion.classList.add('mostrar');
+
+    const tabAnterior = document.querySelector('.actual');
+    if(tabAnterior) {
+        tabAnterior.classList.remove('actual');
+    }
+    const tab = document.querySelector(`[data-paso="${paso}"]`);
+    tab.classList.add('actual');
+}
+function tabs() {
+    const botones = document.querySelectorAll('.tabs button');
+    botones.forEach(boton => {
+        boton.addEventListener('click', function(e) {
+            paso = parseInt(e.target.dataset.paso);
+            mostrarSeccion();
+        });
+    });
+}

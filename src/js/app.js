f294b557d051b79ab891a5d177136e9115f4f416
Jhonathan Nila <nilalimon@gmail.com>
2023-11-23 18:33:15 -0600

    Finaliza consumo de API y se muestran los servicios en la página

--- src/js/app.js
+++ src/js/app.js
@@ -12,6 +12,7 @@ function iniciarApp() {
     botonesPaginador(); //Acomoda los botones del páginador de acuerdo a donde este
     paginaSiguiente();
     paginaAnterior();
+    consultarAPI();
 }
 function mostrarSeccion() {
     const seccionAnterior = document.querySelector('.mostrar');
@@ -69,5 +70,33 @@ function paginaSiguiente() {
         console.log(paso);
         paso++;
         botonesPaginador();
+    });
+}
+async function consultarAPI() {
+    try {
+        const url = 'http://localhost:3000/api/servicios';
+        const resultado = await fetch(url);
+        const servicios = await resultado.json();
+        mostrarServicios(servicios);
+    } catch (error) {
+        console.log(error);
+    }
+}
+function mostrarServicios(servicios) {
+    servicios.forEach(servicio => {
+        const {id, nombre, precio} = servicio;
+        const nombreServicio = document.createElement('P');
+        nombreServicio.classList.add('nombre-servicio');
+        nombreServicio.textContent = nombre;
+        const precioServicio = document.createElement('P');
+        precioServicio.classList.add('precio-servicio');
+        precioServicio.textContent = `$${precio}`;
+        const servicioDiv = document.createElement('DIV');
+        servicioDiv.classList.add('servicio');
+        servicioDiv.dataset.idServicio = id;
+
+        servicioDiv.appendChild(nombreServicio);
+        servicioDiv.appendChild(precioServicio);
+        document.querySelector('#servicios').appendChild(servicioDiv);
     });
 }

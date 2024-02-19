3974d52a1f2037f4067e26d5ed2d8ae140150f46
Jhonathan Nila <nilalimon@gmail.com>
2024-02-19 10:35:11 -0600

    Comienza panel de administrador

--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@ require_once __DIR__ . '/../includes/app.php';
 use MVC\Router;
 use Controllers\APIController;
 use Controllers\CitaController;
+use Controllers\AdminController;
 use Controllers\LoginController;
 
 $router = new Router();
@@ -30,8 +31,10 @@ $router->get('/mensaje', [LoginController::class, 'mensaje']);
 
 // Área Privada
 $router->get('/cita', [CitaController::class, 'index']);
+$router->get('/admin', [AdminController::class, 'index']);
 $router->get('/api/servicios', [APIController::class, 'index']);
 $router->post('/api/citas', [APIController::class, 'guardar']);
+
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
